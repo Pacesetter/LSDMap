7931c219d6029966f89add52637022d276b677b7
Simon Timms <stimms@gmail.com>
2014-05-02 14:20:27 -0600

    Rewriting map layers to make use of control

--- Map/Scripts/Site/Home/Index.js
+++ Map/Scripts/Site/Home/Index.js
@@ -6,12 +6,22 @@ var LSDMap;
         var Index = (function () {
             function Index(container) {
                 this.container = container;
-                var map = L.map('map').setView(L.latLng([51.505, -110.09]), 8);
+                var map = L.map("map").setView(L.latLng([51.505, -110.09]), 8);
 
-                // add an OpenStreetMap tile layer
-                new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                var osm = new L.TileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+                    attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
                 }).addTo(map);
+                var baseLayers = { "OpenStreeMap": osm };
+
+                var box = L.polygon([
+                    L.latLng([51, -110]),
+                    L.latLng([49, -110]),
+                    L.latLng([49, -108]),
+                    L.latLng([51, -108]),
+                    L.latLng([51, -110])], { color: "red", fillOpacity: 0 }).addTo(map);
+                var overlays = { "LSDs": box };
+
+                L.control.layers(baseLayers, overlays).addTo(map);
             }
             return Index;
         })();
--- Map/Scripts/Site/Home/Index.ts
+++ Map/Scripts/Site/Home/Index.ts
@@ -4,12 +4,22 @@
 module LSDMap.Home {
     export class Index {
         constructor(public container:JQuery) {
-            var map = L.map('map').setView(L.latLng([51.505, -110.09]), 8);
-            
-            // add an OpenStreetMap tile layer
-            new L.TileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            var map = L.map("map").setView(L.latLng([51.505, -110.09]), 8);
+
+            var osm = new L.TileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+                attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
             }).addTo(map);
+            var baseLayers = { "OpenStreeMap": osm };
+
+
+            var box = L.polygon([L.latLng([51, -110]),
+                L.latLng([49, -110]),
+                L.latLng([49, -108]),
+                L.latLng([51, -108]),
+                L.latLng([51, -110])], { color: "red", fillOpacity: 0 }).addTo(map);
+            var overlays = { "LSDs": box };
+
+            L.control.layers(baseLayers, overlays).addTo(map);
         }
     }
 }
